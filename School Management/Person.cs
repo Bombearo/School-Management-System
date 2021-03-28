bd2e39dc305480db2c4bcc7f15bbf56fc048eac9
Bombearo <bombearogames@gmail.com>
2021-03-28 14:02:42 +0100

    Add project files.

---
+++ School Management/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Management
+{
+    class Person
+    {
+
+        public Person(string name,DateTime dateOfBirth)
+        {
+            this.Name = name;
+            this.DateOfBirth = dateOfBirth;
+        }
+        
+
+        public string Name{get;private set;}
+        protected DateTime DateOfBirth {get; private set;}
+
+        public int Age
+        {get 
+            {
+                int now = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+                int dob = int.Parse(this.DateOfBirth.ToString("yyyyMMdd"));
+                return (now - dob) / 10000;
+            }
+        }
+    }
+}
---
+++ School Management/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace School_Management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DateTime dob = new DateTime(2008, 6, 1, 7, 47, 0);
+            Person p = new Person("John", dob);
+
+            Console.WriteLine(p.Age);
+        }
+    }
+}
