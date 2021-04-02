ceefc6278d8f49cb4a7261e62a0547bd8aff7a94
Bombearo <bombearogames@gmail.com>
2021-04-02 13:29:16 +0100

    Created basic option logic

--- School Management/Program.cs
+++ School Management/Program.cs
@@ -9,8 +9,8 @@ namespace School_Management
             Console.WriteLine("Hello World!");
             DataAccess db = new DataAccess();
             var pupils = db.GetClasses();
-            TimeSpan t = new TimeSpan(9,30,0);
-            var c = new Course_Class("Computing Science","Advanced Higher",7,3,t,"Wednesday");
+            TimeSpan t = new TimeSpan(9, 30, 0);
+            var c = new Course_Class("Computing Science", "Advanced Higher", 7, 3, t, "Wednesday");
 
             foreach (Course_Class course in pupils)
             {
@@ -18,6 +18,41 @@ namespace School_Management
 
             }
             c.AddSelf();
+            HomePage();
+
         }
+
+        private static void ShowOptions()
+        {
+            int choice = 0;
+            bool running = true;
+            do
+            {
+
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out choice) && input == ""){
+                    choice = -1;
+                }
+                switch (choice)
+                {
+                    case -1: 
+                        running = false;
+                        break;
+                    case 1:
+                        break;
+                    default:
+                        Console.WriteLine("Your input was not valid, please enter a correct number or nothing to exit.");
+                        break;
+                }
+
+            } while (running);
+        }
+
+        private static void HomePage()
+        {
+            Console.WriteLine("Enter nothing to quit");
+            ShowOptions();
+        }
+
     }
 }
